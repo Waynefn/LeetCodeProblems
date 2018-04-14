b9b7d39f505dc375db18720093debbbcd2911228
zhixin_li <31786852+Waynefn@users.noreply.github.com>
2018-04-14 20:17:28 +0900

    Create 027.c

---
+++ 027.c
@@ -0,0 +1,8 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int len=0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=val)
+            nums[len++]=nums[i];
+    }
+    return len;
+}
